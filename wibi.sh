42f50c07362de3fc3d9b691852e68d44d0d0ea75
WhoMHw <71681090+WhoMHw@users.noreply.github.com>
2020-10-22 23:25:22 +0700

    Add files via upload

---
+++ wibi.sh
@@ -0,0 +1,133 @@
+#!/bin/bash
+
+echo "Hallo kaka,siapa nama kaka ?" #tulisan keluar
+read nick #membaca tulisan
+echo -e "\e[1;32m
+                ──▄▀▀▀▄───────────────
+                Lu Harus Tau Passwordnya :)
+                ──█───█───────────────
+                ─███████─────────▄▀▀▄─
+                ░██─▀─██░░█▀█▀▀▀▀█░░█░
+                ░███▄███░░▀░▀░░░░░▀▀░░
+\e[0m"
+read -p $'\e[32mInput Username :\e[0m ' user
+read -s -p $'\e[32mInput Password :\e[0m ' pass
+if [[ $pass == Gans && $user == MHw ]]; then
+figlet SELAMAT DATANG | lolcat
+shopt -s autocd
+shopt -s cdspell
+shopt -s checkhash
+shopt -s checkwinsize
+shopt -s compat31
+shopt -s compat32
+shopt -s compat40
+shopt -s compat41
+shopt -s no_empty_cmd_completion
+shopt -s histverify
+shopt -s histappend
+shopt -s dirspell
+shopt -s direxpand
+shopt -s compat43
+shopt -s compat32
+shopt -s lithist
+else
+echo -e "\e[1;31m  Lo salah,Kalau mau pinjam bilang orangnya dulu TOd
+Bye :)\e[0m"
+sleep 3
+termux-open https://wa.me/+6285336515046?text=Bg+Bagi+Passwordnya+
+exit
+fi
+trap 2
+echo "Follow IG ku ya..!" $nick
+sleep 2
+echo "nanti klo enggak follow dosa lo"
+termux-open https://www.instagram.com/mhw_______wb23/
+clear
+figlet WhoMHw | lolcat
+echo "jangan direcode ya"
+sleep 2
+echo "hack cctv"
+sleep 3
+clear
+figlet HACK CCTV |lolcat
+echo "macam² CCTV dari Berbagai Negara"
+sleep 2
+echo "tunggu ya.........!"
+sleep 3
+echo "================================WhoMHw÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷"
+sleep 2
+echo "1.http://119.2.50.116:90"
+sleep 2
+echo "2.http://119.2.50.116:83"
+sleep 1
+echo "3.http://119.2.50.116:89"
+echo "4.http://203.130.227.59:8081"
+sleep 1
+echo "5.http://203.201.173.172:60001"
+sleep 1
+echo "6.http://203.130.227.59:8084"
+sleep 2
+echo "7.http://203.130.227.59:8083"
+sleep 2
+echo "8.http://27.123.4.106:8082"
+sleep 2
+echo "9.http://222.124.156.68:8084"
+sleep 1
+echo "10.http://119.2.50.116:8082 "
+sleep 2
+echo "11.http://115.124.85.163:83"
+sleep 1
+echo "12.http://119.252.169.189:85"
+sleep 2
+echo "13.http://36.66.133.249:80"
+sleep 2
+echo "Subscribe juga channel temen ku ya"
+termux-open https://m.youtube.com/channel/UCTMBlJoMW9jaWyiv8oZsjKw
+echo "14. http://61.90.208.5:8888"
+sleep 3
+echo "15.http://180.222.158.52:60001"
+echo "16.http://82.205.114.65:60001"
+sleep 3
+echo "17.http://119.2.50.116:84"
+sleep 4
+echo "18.http://119.197.80.97:84"
+sleep 2
+echo "19.http://119.197.80.97:91"
+echo "20.http://119.197.80.97:83"
+echo "21.http://119.197.80.97:88"
+echo "22.http://119.2.50.116:89"
+echo "23.http://119.2.50.116:8082"
+sleep 3
+echo "24.http://119.252.169.189:85"
+sleep 2
+echo "25. http://36.66.133.249:80"
+sleep 2
+echo "26.http://118.136.178.143:60001"
+sleep 3
+echo "27.http://43.252.105.62:89"
+sleep 2
+echo "28.http://119.11.196.42:82"
+sleep 3
+echo "29.http://119.252.169.189:83"
+sleep 2
+echo "30.http://119.252.169.189:82"
+echo "31.http://43.251.99.210:60001"
+sleep 2
+echo "32.http://101.128.84.198:60001"
+sleep 1
+echo "33.http://119.2.50.116:8082"
+sleep 1
+echo "34.http://103.230.48.172:89"
+sleep 2
+echo "35.http://222.124.156.54:80"
+sleep 3
+echo "÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷"
+sleep 1
+echo "			_Author:WhoMHw					    "
+sleep 2
+echo "÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷"
+sleep 2
+echo "cara pakainya salin salah satu dan pastekan dichroom"
+figlet TERIMA KASIH TELAH MENGGUNAKAN TOOL GABUT | lolcat
+
+
